5b372de19ef848d81fc4639d26044a1ae17528f8
Molnár Vilmos <info@vilibook.hu>
2015-08-02 19:12:12 +0200

    Create user_timeline.php

---
+++ menus/user_timeline.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '<ul>';
+$i = 0;
+foreach ($params['menu'] as $menu) {
+    if ($i <= 3) {
+        foreach ($menu as $name => $link) {
+            echo "<li><a href='{$link}'><span>" . ossn_print($name) . "</span></a></li>";
+        }
+    } else {
+        echo "<li><a href='{$links}'>" . ossn_print('more') . "</a>
+		  <ul>";
+        foreach ($menu as $name => $link) {
+            echo "<li><a href='{$link}'><span>" . ossn_print($name) . "</span></a></li>";
+        }
+        echo "</ul>
+		 </li>";
+    }
+    $i++;
+}
+echo '</ul>';
