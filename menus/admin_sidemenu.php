03b5935f7720b6e0ab58318702938679e9a723cf
Molnár Vilmos <info@vilibook.hu>
2015-08-02 19:14:48 +0200

    Create admin_sidemenu.php

---
+++ menus/admin_sidemenu.php
@@ -0,0 +1,19 @@
+<?php
+
+echo '<div class="ossn-admin-sidemenu">';
+foreach ($params['menu'] as $value) {
+    foreach ($value as $key => $value) {
+        echo "<div class='title'>" . strtoupper($key) . "</div>";
+        echo "<div class='links'>";
+        foreach ($value as $name => $link) {
+            $name_link = ossn_print($name);
+            $icon = str_replace(':', '-', $name);
+            $active_theme = ossn_site_settings('theme');
+            $icon = ossn_site_url("themes/{$active_theme}/images/administrator/icons/{$icon}.png");
+            $icon = "<div class='icon' style='background:url(\"{$icon}\") no-repeat;'></div>";
+            echo "<a href='{$link}'><li><div class='inner-li'>{$icon}<div class='text'>{$name_link}</div></div></li></a>";
+        }
+        echo '</div>';
+    }
+}
+echo '</div>';
